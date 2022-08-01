51d1a68dfb400565fb898f721761686873daac26
fidyahptr <fidyahputri15@gmail.com>
2022-08-01 16:34:40 +0700

    Update Guest Page

--- src/customApp/containers/blankPage.js
+++ src/customApp/containers/blankPage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import basicStyle from "../../settings/basicStyle";
 import LayoutContentWrapper from "../../components/utility/layoutWrapper.js";
 import LayoutContent from "../../components/utility/layoutContent";
 import Input from '../../components/uielements/input';
 import Button from '../../components/uielements/button';
 import IntlMessages from "../../components/utility/intlMessages";
 import Card from '../components/Card';
-import { Form } from 'antd';
+import { Form, Row, Col } from 'antd';
 
 const BlankPage = () => {
   const [data, setData] = useState([]);
@@ -31,10 +32,7 @@ const BlankPage = () => {
     setInput(result);
   };
 
-  const handleUpdate = e => {
-    setUpdate(e.target.value);
-  };
-
+  // Submit Booking Code 
   const handleSubmit = async event => {
     event.preventDefault();
 
@@ -52,7 +50,8 @@ const BlankPage = () => {
     setSubmit(true);
   };
 
-  function updatePost() {
+  // update Arrival Time
+  const updatePost = () => {
     axios
       .put(`https://bv-online-assessment.herokuapp.com/api/bookings/${input}/update-eta`, {
         booking_code: input,
@@ -60,41 +59,56 @@ const BlankPage = () => {
       })
       .then((response) => {
         setData2(response.data);
-        console.log(response.data);
-
-      });
+        alert("Data Berhasil di Update!")
+      })
+      .catch(err => alert("Data Gagal di Update!"));
+    ;
   }
+
+  // input update arrival time
+  const handleUpdate = e => {
+    setUpdate(e.target.value);
+  };
+
+  // row col
+  const { rowStyle, colStyle, gutter } = basicStyle;
 
   return (
     <div>
       <LayoutContentWrapper style={{ height: "100vh" }}>
         <LayoutContent>
-          <Form onSubmit={handleSubmit}>
-            <IntlMessages id="text.bookingCode" />
+          <Row style={rowStyle} gutter={gutter} justify="center">
+            <Col md={12} sm={12} xs={24} style={colStyle}>
+              <Form onSubmit={handleSubmit}>
+                <IntlMessages id="text.bookingCode" />
 
-            <Input placeholder="KBU2127" name="kode" value={input} onChange={handleChange} pattern="[A-Z0-9]+"/>
-            <Button type="primary" htmlType="submit">
-              Search
+                <Input placeholder="KBU2127" name="kode" value={input} onChange={handleChange} pattern="[A-Z0-9]+" />
+                <Button type="primary" htmlType="submit">
+                  Search
           </Button>
-          </Form>
-          {submit && (
-            data2 ? (
-              <div>
-                <Card guest_name={data2.guest_name} property_name={data2.property_name} check_in_date={data2.check_in_date} check_out_date={data2.check_out_date} arrival_time={data2.arrival_time} profile_picture={data2.profile_picture} />
-                <label for="appt">Choose a time for your meeting:</label>
+              </Form>
+              {submit && (
+                data2 ? (
+                  <div>
+                    <Card guest_name={data2.guest_name} property_name={data2.property_name} check_in_date={data2.check_in_date} check_out_date={data2.check_out_date} arrival_time={data2.arrival_time} profile_picture={data2.profile_picture} />
+                    <Row style={rowStyle} gutter={gutter} justify="start">
+                      <Col md={12} sm={12} xs={24} style={colStyle}>
+                        <label for="appt">Want to update your arrival time?</label>
+                        <input type="time" id="appt" name="appt" value={update} onChange={handleUpdate} />
+                        <Button size="small" type="primary" onClick={updatePost}>Update Post</Button>
+                      </Col>
+                    </Row>
 
-                <input type="time" id="appt" name="appt" value={update} onChange={handleUpdate} />
+                  </div>
+                ) : (
+                    <div>
+                      <p>Tidak ada Kode Booking</p>
+                    </div>
+                  )
 
-                <small>Office hours are 9am to 6pm</small>
-                <button onClick={updatePost}>Update Post</button>
-              </div>
-            ) : (
-                <div>
-                  <p>Tidak ada Kode Booking</p>
-                </div>
-              )
-
-          )}
+              )}
+            </Col>
+          </Row>
         </LayoutContent>
       </LayoutContentWrapper>
     </div>
